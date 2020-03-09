8e017c9faac3af4251346ce9042a80aac9dde017
Javier Ramirez <apollo.infinity88@gmail.com>
2020-03-09 08:59:45 -0700

    Changed tutorial for better understanding of how Django works

--- trydjango/trydjango/urls.py
+++ trydjango/trydjango/urls.py
@@ -1,7 +1,7 @@
-"""mysite URL Configuration
+"""trydjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from pages.views import home_view, contact_view, about_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
-    path('polls', include('polls.urls')),  # runs polls/urls.py
+    path('', home_view, name='home'),
+    path('contact/', contact_view),
+    path('about/', about_view),
+    path('create/', product_create_view),
+    path('product/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
